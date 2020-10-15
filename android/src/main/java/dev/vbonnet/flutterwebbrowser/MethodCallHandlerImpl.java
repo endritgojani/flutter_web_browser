c691d4ee6dbfc3c8b7219f00b92ea5172fad16e5
Endrit Gojani <>
2020-10-15 15:34:22 +0200

    no message

--- android/src/main/java/dev/vbonnet/flutterwebbrowser/MethodCallHandlerImpl.java
+++ android/src/main/java/dev/vbonnet/flutterwebbrowser/MethodCallHandlerImpl.java
@@ -6,52 +6,54 @@ import android.net.Uri;
 import android.webkit.WebView;
 
 import androidx.browser.customtabs.CustomTabsIntent;
-
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 public class MethodCallHandlerImpl implements MethodCallHandler {
 
-    private Activity activity;
+  private Activity activity;
 
-    public void setActivity(Activity activity) {
-        this.activity = activity;
+  public void setActivity(Activity activity) {
+    this.activity = activity;
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    switch (call.method) {
+      case "openWebPage":
+        openUrl(call, result);
+        break;
+      default:
+        result.notImplemented();
+        break;
     }
+  }
 
-    @Override
-    public void onMethodCall(MethodCall call, Result result) {
-        switch (call.method) {
-            case "openWebPage":
-                openUrl(call, result);
-                break;
-            default:
-                result.notImplemented();
-                break;
-        }
+  private void openUrl(MethodCall call, Result result) {
+    if (activity == null) {
+      result.error("no_activity", "Plugin is only available within a activity context", null);
+      return;
     }
+    String url = call.argument("url");
+    String toolbarColorArg = call.argument("android_toolbar_color");
 
-    private void openUrl(MethodCall call, Result result) {
-        if (activity == null) {
-            result.error("no_activity", "Plugin is only available within a activity context", null);
-            return;
-        }
-        String url = call.argument("url");
-        String toolbarColorArg = call.argument("android_toolbar_color");
+    WebView webView = new WebView(activity);
+    webView.getSettings().setJavaScriptEnabled(true);
+    String pdf = "http://www.adobe.com/devnet/acrobat/pdfs/pdf_open_parameters.pdf";
+    webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + pdf);
 
-        WebView webView = new WebView(activity);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=" + url);
+
 
 //
-//        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-//        if (toolbarColorArg != null) {
-//            int toolbarColor = Color.parseColor(toolbarColorArg);
-//            builder.setToolbarColor(toolbarColor);
-//        }
-//        CustomTabsIntent customTabsIntent = builder.build();
-//        customTabsIntent.launchUrl(activity, Uri.parse("https://drive.google.com/viewerng/viewer?embedded=true&url=" + url));
+//    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+//    if (toolbarColorArg != null) {
+//      int toolbarColor = Color.parseColor(toolbarColorArg);
+//      builder.setToolbarColor(toolbarColor);
+//    }
+//    CustomTabsIntent customTabsIntent = builder.build();
+//    customTabsIntent.launchUrl(activity, Uri.parse("https://drive.google.com/viewerng/viewer?embedded=true&url="+url));
 
-        result.success(null);
-    }
+    result.success(null);
+  }
 }
